4d8c4e829789bb10b442f3d5f698ceb87f7b9cf8
Samuel Bueno Francisco <samuel.buenofran@gmail.com>
2024-03-06 15:35:36 -0300

    First activity completed, added 5 fully functional exercises from the list.

---
+++ src/userui/PainelCirc.java
@@ -0,0 +1,72 @@
+package userui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PainelCirc extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField textFieldRadius;
+	private JTextField textFieldResults;
+
+	/**
+	 * Create the panel.
+	 * 
+	 */
+	public PainelCirc() {
+		setLayout(null);
+
+		textFieldRadius = new JTextField();
+		textFieldRadius.setBounds(52, 74, 86, 20);
+		add(textFieldRadius);
+		textFieldRadius.setColumns(10);
+
+		JLabel lblRadius = new JLabel("Radius:");
+		lblRadius.setBounds(52, 49, 46, 14);
+		add(lblRadius);
+
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double radius = Double.parseDouble(textFieldRadius.getText());
+				double area = 0;
+				double piValue = Math.PI;
+
+				area = piValue * Math.pow(radius, 2);
+				DecimalFormat df = new DecimalFormat("#.##");
+				String formattedResult = df.format(area);
+
+				textFieldResults.setText(formattedResult);
+
+			}
+		});
+		btnCalculate.setBounds(52, 229, 89, 23);
+		add(btnCalculate);
+
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textFieldRadius.setText("");
+				textFieldRadius.requestFocus();
+			}
+		});
+		btnClear.setBounds(206, 229, 89, 23);
+		add(btnClear);
+
+		textFieldResults = new JTextField();
+		textFieldResults.setBounds(52, 145, 86, 20);
+		add(textFieldResults);
+		textFieldResults.setColumns(10);
+
+		JLabel lblResults = new JLabel("Results:");
+		lblResults.setBounds(52, 119, 46, 14);
+		add(lblResults);
+
+	}
+}
---
+++ src/userui/PainelQuad.java
@@ -0,0 +1,95 @@
+package userui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PainelQuad extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField textFieldSides;
+	private JTextField textFieldArea;
+	private JTextField textFieldDouble;
+
+	/**
+	 * Create the panel.
+	 */
+	public PainelQuad() {
+		setLayout(null);
+
+		JLabel lblSides = new JLabel("Sides:");
+		lblSides.setBounds(59, 65, 46, 14);
+		add(lblSides);
+
+		textFieldSides = new JTextField();
+		textFieldSides.setBounds(59, 90, 86, 20);
+		add(textFieldSides);
+		textFieldSides.setColumns(10);
+
+		textFieldArea = new JTextField();
+		textFieldArea.setBounds(59, 174, 86, 20);
+		add(textFieldArea);
+		textFieldArea.setColumns(10);
+
+		JLabel lblArea = new JLabel("Area:");
+		lblArea.setBounds(59, 149, 46, 14);
+		add(lblArea);
+
+		JLabel lblDouble = new JLabel("Double:");
+		lblDouble.setBounds(198, 149, 46, 14);
+		add(lblDouble);
+
+		textFieldDouble = new JTextField();
+		textFieldDouble.setBounds(198, 174, 86, 20);
+		add(textFieldDouble);
+		textFieldDouble.setColumns(10);
+
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double sides = Double.parseDouble(textFieldSides.getText());
+				DecimalFormat df = new DecimalFormat("#.##");
+				// Alternative
+				// double area = 0d;
+				// area = sides * sides;
+
+				// Calculates the area
+				sides *= sides;
+
+				String formattedResultArea = df.format(sides);
+
+				textFieldArea.setText(formattedResultArea);
+
+				// Doubles the area
+				sides *= 2;
+
+				String formattedResultDouble = df.format(sides);
+
+				textFieldDouble.setText(formattedResultDouble);
+
+			}
+		});
+		btnCalculate.setBounds(59, 232, 89, 23);
+		add(btnCalculate);
+
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textFieldSides.setText("");
+				textFieldArea.setText("");
+				textFieldDouble.setText("");
+				textFieldSides.requestFocus();
+
+			}
+		});
+		btnClear.setBounds(198, 232, 89, 23);
+		add(btnClear);
+
+	}
+
+}
